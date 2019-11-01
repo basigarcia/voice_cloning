666a09a82ae8174aea34b65bf139ea5daa2c0382
Yiyang Zeng <674977156@qq.com>
2019-11-01 15:53:38 -0400

    Create a argument parser for encoder test

---
+++ encoder_test.py
@@ -0,0 +1,26 @@
+from utils.argutils import print_args
+from encoder.test import test
+from pathlib import Path
+import argparse
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Test models against the test dataset. You must have trained or downloaded models first.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    
+    parser.add_argument("run_id", type=str, help= \
+        "Name for this model instance. It must be a already trained model")
+    parser.add_argument("test_data_root", type=Path, help= \
+        "Path to the output directory of encoder_preprocess.py. If you left the default "
+        "output directory when preprocessing, it should be <datasets_root>/SV2TTS/encoder/.")
+    args = parser.parse_args()
+    
+    # Process the arguments
+    args.models_dir.mkdir(exist_ok=True)
+    
+    # Run the training
+    print_args(args, parser)
+    train(**vars(args))
+    
