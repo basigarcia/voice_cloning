aa6a9f8fd769555f053417d6ed60ed2756185b18
Yiyang Zeng <yz3622@nyu.edu>
2019-12-01 11:26:33 -0500

    Create all_datasets_pretrained_benchmark.sh

---
+++ all_datasets_pretrained_benchmark.sh
@@ -0,0 +1,4 @@
+python encoder_test.py pretrained ./test_data/LibriSpeechClean_test/encoder/
+python encoder_test.py pretrained ./test_data/LibriSpeechOther_test/encoder/
+python encoder_test.py pretrained ./test_data/VoxCeleb1_test/encoder/
+python encoder_test.py pretrained ./test_data/VoxCeleb2_test/encoder/
